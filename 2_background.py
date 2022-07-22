2cc2992937fbff2156b42164aafdd992cb0a26e2
ZHspacecode0707 <109786678+ZHspacecode0707@users.noreply.github.com>
2022-07-22 16:19:21 +0900

    python game
    asdf

---
+++ 2_background.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.init()#초기화
+
+#화면 크기
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width,screen_height))
+
+
+#화면 타이틀
+pygame.display.set_caption('kimsd')
+#배경이미지 불러오기
+background = pygame.image.load('C:/Users/SungYul Kim/Desktop/파이썬 공부  파일/gameproject_pygame/background.png')
+
+#이벤트 루프
+running = True
+while running:
+    for event in pygame.event.get():#어떤 이벤트가 발생했는가?
+        if event.type == pygame.QUIT:#창 닫을 시
+            running = False#게임이 진행중이 아님
+
+    #screen.fill((0, 0, 255))
+    screen.blit(background, (0, 0))#배경 그리기
+
+    pygame.display.update()#게임 화면을 다시 그리기!
+            
+#pygame 종료
+pygame.quit()
